8d3074eeec2862a0b9dd65d2cda21b5cae67c673
jela3105 <jela3105@gmail.com>
2020-12-22 20:19:54 -0600

    Add first ListView

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Components',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Components'),
-        ),
-        body: HomePageTemp(),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePageTemp());
   }
 }
--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Components temp')),
+      body: ListView(
+        children: <Widget>[
+          ListTile(title: Text('ListTile')),
+          Divider(),
+          ListTile(title: Text('ListTile')),
+          Divider(),
+        ],
+      ),
     );
   }
 }
