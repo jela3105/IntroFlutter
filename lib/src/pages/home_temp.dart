4a640771aaa47c20ad424ba5ade42748d5d4c212
jela3105 <jela3105@gmail.com>
2020-12-23 13:19:07 -0600

    Create ListTiles from a static List

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final options = ['One', 'Two', 'Three', 'Four', 'Five'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Components temp')),
-      body: ListView(
-        children: <Widget>[
-          ListTile(title: Text('ListTile')),
-          Divider(),
-          ListTile(title: Text('ListTile')),
-          Divider(),
-        ],
-      ),
+      body: ListView(children: _createItems()),
     );
+  }
+
+  List<Widget> _createItems() {
+    List<Widget> list = new List<Widget>();
+    for (String option in options) {
+      final tempWidget = ListTile(
+        title: Text(option),
+      );
+
+      list..add(tempWidget)..add(Divider());
+    }
+    return list;
   }
 }
