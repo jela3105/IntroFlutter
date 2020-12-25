53672c73caef1a07632e98502c439ecf1b99074d
jela3105 <jela3105@gmail.com>
2020-12-25 01:15:20 -0600

    Add stateful widget home_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:components/src/pages/home_temp.dart';
+import 'package:components/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePageTemp());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
