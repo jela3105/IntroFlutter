7114e854fb84b157c545927ce0769305f2b68e16
jela3105 <jela3105@gmail.com>
2020-12-27 23:54:59 -0600

    Create independet file for routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:components/src/pages/home_page.dart';
 import 'package:components/src/pages/alert_page.dart';
-import 'package:components/src/pages/avatar_page.dart';
+import 'package:components/src/routes/routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,11 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
-      },
+      routes: getApplicationRoutes(),
       onGenerateRoute: (RouteSettings settings) {
         return MaterialPageRoute(
             builder: (BuildContext context) => AlertPage());
