0494445c2eef722fe348d4f1f0d37a5003a10b8c
jela3105 <jela3105@gmail.com>
2020-12-30 22:56:02 -0600

    Finish example in avatars page

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,28 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 9.0),
+            child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://yt3.ggpht.com/ytc/AAUvwniSOnUfYbdRPYEk0I4GqF16MW8YxZYjHR1o14CGfw=s900-c-k-c0x00ffffff-no-rj')),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('JL'),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          fadeInDuration: Duration(milliseconds: 200),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(
+              'https://rtvc-assets-radionica3.s3.amazonaws.com/s3fs-public/styles/image_750x424/public/field/image/article/twentyone_pilots-trench.jpg?itok=YkmWss1Y'),
+        ),
       ),
     );
   }
