dac536652427c84e53483f5146c06c1c035151cd
jela3105 <jela3105@gmail.com>
2021-01-02 23:10:46 -0600

    Add Scaffold widget and Container properties

--- lib/src/pages/animated_container.dart
+++ lib/src/pages/animated_container.dart
@@ -6,8 +6,23 @@ class AnimatedContainerPage extends StatefulWidget {
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+  double _width = 50.0;
+  double _height = 50.0;
+  Color _color = Colors.blueAccent;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animated Container'),
+      ),
+      body: Center(
+          child: Container(
+        width: _width,
+        height: _height,
+        decoration: BoxDecoration(color: _color, borderRadius: _borderRadius),
+      )),
+    );
   }
 }
