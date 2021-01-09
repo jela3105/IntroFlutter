722e9a20819fd7eb1cbfb9d29d499cb28d24b739
jela3105 <jela3105@gmail.com>
2021-01-08 20:36:44 -0600

    Create Slider Page

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slider'),
+      ),
+      body: Container(
+        child: Text('Slider Page'),
+      ),
+    );
+  }
+}
