af4c7f95a8387ab7f21d0f930d36ee5710cede5a
jela3105 <jela3105@gmail.com>
2021-01-10 21:22:05 -0600

    Use ListView builder

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -6,13 +6,29 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  List<int> _numberList = [1, 2, 3, 4, 5];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lists'),
       ),
-      body: Text('List...'),
+      body: _createList(),
+    );
+  }
+
+  Widget _createList() {
+    return ListView.builder(
+      itemCount: _numberList.length,
+      itemBuilder: (BuildContext context, int index) {
+        final image = _numberList[index];
+
+        return FadeInImage(
+          image: NetworkImage('http://picsum.photos/500/300/?image=$image'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        );
+      },
     );
   }
 }
